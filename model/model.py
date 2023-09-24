08145268103ac1a53a223060327657ecc2c66e7c
andreishr <andreighiteanu1@gmail.com>
2023-09-24 20:10:52 +0300

    python 310 dependency error solved

--- model/model.py
+++ model/model.py
@@ -1,3 +1,14 @@
-from parsingttl import get_interval_rdfs
+from parsingttl import graph, get_interval_rdfs
+from pyrdf2vec import RDF2VecTransformer
+from pyrdf2vec.embedders import Word2Vec
+from pyrdf2vec.graphs import KG
+from pyrdf2vec.walkers import RandomWalker
+# print(get_interval_rdfs())
 
-print(get_interval_rdfs())
+
+for subject, predicate, obj in graph:
+        if predicate.split('#')[1] == 'hasInterval':
+            print(f"Subject: {subject}")
+            print(f"Predicate: {predicate}")
+            print(f"Object: {obj}")
+            
