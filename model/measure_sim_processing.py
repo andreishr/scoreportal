7f16b567ac17e3834067232998a3e5283e20798c
andreishr <andreighiteanu1@gmail.com>
2024-04-14 20:08:08 +0300

    added 2 more factors

--- model/measure_sim_processing.py
+++ model/measure_sim_processing.py
@@ -3,6 +3,9 @@ from termcolor import  cprint
 import os
 from dotenv import load_dotenv
 import ast
+import math
+import numpy as np
+
 
 from classes.chord import Chord
 from classes.measure import Measure
@@ -103,7 +106,11 @@ def get_element_similarity(elements, verbose = 0):
 def get_sim_score(elem1: Measure, elem2: Measure):
     sc =  get_sc_score(elem1.chords, elem2.chords, "list")
     wc = 0.4
-    sm = wc * sc
+    sml = get_sml_score(elem1.duration, elem2.duration)
+    wml = 0.1
+    sr = get_sr_score(elem1.chords, elem2.chords)
+    wsr = 0.3
+    sm = wc * sc + wml * sml + sr * wsr
     return sm
 
 def get_sc_score(chord_list1, chord_list2, byCriteria: str):
@@ -113,5 +120,38 @@ def get_sc_score(chord_list1, chord_list2, byCriteria: str):
 
         return len(common_chords) / len(chord_list1 + chord_list2)
     
+def get_sml_score(duration1, duration2):
+
+    d1_float = float(duration1[0])
+    d2_float = float(duration2[0])
+
+    ml_diff = 0
+    if d1_float > d2_float:
+        ml_diff = d1_float - d2_float
+    else:
+        ml_diff = d2_float - d1_float
+
+    return 1 if ml_diff == 0 else (1 - (ml_diff / (10 ** math.ceil(math.log10(ml_diff)))))
+
+def get_sr_score(chord_list1, chord_list2):
+
+    r_vec1 = [float(chord.duration[0]) for chord in chord_list1]
+    r_vec2 = [float(chord.duration[0]) for chord in chord_list2]
+    
+    len_v1 = len(r_vec1)
+    len_v2 = len(r_vec2)
+
+    if len_v1 > len_v2:
+        r_vec2 = np.concatenate([r_vec2, np.zeros(len_v1 - len_v2)])
+    elif len_v2 > len_v1: 
+        r_vec1 = np.concatenate([r_vec1, np.zeros(len_v2 - len_v1)])
+    
+    dot_p = np.dot(r_vec1, r_vec2)
+    norm_v1 = np.linalg.norm(r_vec1)
+    norm_v2 = np.linalg.norm(r_vec2)
+
+    return dot_p / (norm_v1 * norm_v2)
+
+
 get_element_similarity(measure_object_list[:15], 1)
 
