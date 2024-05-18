d9393da66599fb537c0922de24ea2126411a177c
andreishr <andreighiteanu1@gmail.com>
2024-05-18 17:37:52 +0300

    intermediate update

--- model/measure_sim_processing.py
+++ model/measure_sim_processing.py
@@ -6,6 +6,7 @@ import ast
 import math
 import numpy as np
 from scipy.integrate import quad
+import sympy as smp
 
 from classes.chord import Chord
 from classes.measure import Measure
@@ -31,6 +32,12 @@ measure_attributes = {}
 
 chord_set = set()
 chord_attributes = {}
+
+def write_to_file(file, list_of_tuples):
+    with open(file, 'w') as file:
+        for tpl in list_of_tuples:
+            line = ','.join(map(str, tpl))
+            file.write(line + '\n') 
 
 # Getting all measues and chords
 for s, p, o in graph:
@@ -90,12 +97,15 @@ def get_element_similarity(elements, verbose = 0):
     individuals_tuple_list_integral = []
     checks = set()
     for first_element in elements:
+        print("STEP...")
         for second_element in elements:
             if first_element != second_element and (first_element, second_element) not in checks and (second_element, first_element) not in checks:
                 similarity = get_sim_score(first_element, second_element)
-                similarity_integral = get_mes_sim_integral_based(first_element, second_element)
+                if len(first_element.duration) > 0 and len (second_element.duration) > 0:
+                    if (first_element.duration[0] == second_element.duration[0]):
+                        similarity_integral = get_mes_sim_integral_based(first_element, second_element)
+                        individuals_tuple_list_integral.append((first_element, second_element, similarity_integral))
                 individuals_tuple_list.append((first_element, second_element, similarity))
-                individuals_tuple_list_integral.append((first_element, second_element, similarity_integral))
             checks.add((first_element, second_element))
 
     individuals_tuple_list.sort(key=lambda x: x[2], reverse=True)
@@ -108,22 +118,37 @@ def get_element_similarity(elements, verbose = 0):
             cprint(similarity_tuple[2], 'green', attrs=["bold"])
 
         print()
-    # integral_values = [v[2] for v in individuals_tuple_list_integral]
-    # min_val = min(integral_values)
-    # max_val = max(integral_values)
-    # scaled_list = [(vs[0], vs[1], (vs[2] - min_val) / (max_val - min_val)) for vs in individuals_tuple_list_integral]
-    # if verbose != 0:
-        for similarity_tuple in individuals_tuple_list_integral[:5]:
+    integral_values = [v[2] for v in individuals_tuple_list_integral]
+    min_val = 0
+    max_val = max(integral_values)
+    print(min_val)
+    print(max_val)
+    scaled_list = [(vs[0], vs[1], (vs[2] - min_val) / (max_val - min_val)) for vs in individuals_tuple_list_integral]
+    if verbose != 0:
+        # for similarity_tuple in individuals_tuple_list_integral[:30]:
+        #     cprint(similarity_tuple[0].name, 'yellow', attrs=["bold"])
+        #     cprint(similarity_tuple[1].name, 'yellow', attrs=["bold"])
+        #     cprint(similarity_tuple[2], 'red', attrs=["bold"])
+        cprint("UNSCALED:", "blue")
+        for similarity_tuple in scaled_list[:100]:
             cprint(similarity_tuple[0].name, 'yellow', attrs=["bold"])
             cprint(similarity_tuple[1].name, 'yellow', attrs=["bold"])
             cprint(similarity_tuple[2], 'red', attrs=["bold"])
+
+    # write_to_file("not_scaled", [(similarity_tuple[0].name, similarity_tuple[1].name, similarity_tuple[2]) for similarity_tuple in individuals_tuple_list_integral])
+    # write_to_file("scaled", [(similarity_tuple[0].name, similarity_tuple[1].name, similarity_tuple[2]) for similarity_tuple in scaled_list])
+
+    
     return individuals_tuple_list
     
 
 def get_sim_score(elem1: Measure, elem2: Measure):
+    #Todo aici conditie
     sc =  get_sc_score(elem1.chords, elem2.chords, "list")
     wc = 0.4
-    sml = get_sml_score(elem1.duration, elem2.duration)
+    sml = 0
+    if len(elem1.duration) > 0 and len(elem2.duration) > 0:
+        sml = get_sml_score(elem1.duration, elem2.duration)
     wml = 0.1
     sr = get_sr_score(elem1.chords, elem2.chords)
     wsr = 0.3
@@ -140,7 +165,6 @@ def get_sc_score(chord_list1, chord_list2, byCriteria: str):
         return len(common_chords) / len(chord_list1 + chord_list2)
     
 def get_sml_score(duration1, duration2):
-
     d1_float = float(duration1[0])
     d2_float = float(duration2[0])
 
@@ -179,9 +203,7 @@ def get_spd_score(chord_list1, chord_list2):
         jaccard_vect2 = []
         for value in all_values:
             if(value not in checked_values):
-                # Check if the value is in list1
                 in_list1 = any(value in obj.pitches for obj in chord_list1)
-                # Check if the value is in list2
                 in_list2 = any(value in obj.pitches for obj in chord_list2)
 
                 jaccard_vect1.append(1 if in_list1 else 0)
@@ -203,11 +225,6 @@ def jaccard_binary(x,y):
     return similarity
 
 
-
-# for measure in measure_attributes:
-#     print(measure, measure_attributes[measure])
-
-
 """
 To be implemented a function of frequency:
 f(x)=A*sin(2pi*f*x)
@@ -218,69 +235,83 @@ Assumtions:
 
 """
 
+def h(x):
+    return abs(math.sin(2*math.pi*x))
+
 def g(x, c_list):
     sum_of_f = 0
     if isinstance(c_list, list):
         for c in c_list:
-            sum_of_f += math.sin(2*math.pi*float(c)/100*x)
+            sum_of_f += np.sin(2*math.pi*float(c)*x)
     else:
-        sum_of_f += math.sin(2*math.pi*float(c_list)/100*x)
+        sum_of_f += np.sin(2*math.pi*float(c_list)*x)
     return sum_of_f
 
 def f(x, c_list):
     sum_of_f = 0
     if isinstance(c_list, list):
         for c in c_list:
-            sum_of_f += math.sin(2*math.pi*float(c)*x)
+            sum_of_f += np.sin(2*math.pi*float(c)*x)
     else:
-        sum_of_f += math.sin(2*math.pi*float(c_list)/100*x)
+        sum_of_f += np.sin(2*math.pi*float(c_list)*x) 
     return sum_of_f
+
+def diff_f(x, c_list1, c_list2):
+    return abs(f(x, c_list1) - g(x, c_list2))
+
 
 def get_mes_sim_integral_based(measure_element1, measure_element2):
     integral_score = 0
     sorted_chords_m1 = sorted(measure_element1.chords, key=lambda x: x.offset)
     sorted_chords_m2 = sorted(measure_element2.chords, key=lambda x: x.offset)
-    if measure_element1.duration[0] == measure_element2.duration[0]:
-        offsets1 = []
-        offsets2 = []
-        durations1 = []
-        durations2 = []
-        freqs1 = []
-        freqs2 = []
-        for chordm1 in sorted_chords_m1:
-            offsets1.append(float(chordm1.offset[0]))
-            durations1.append(float(chordm1.duration[0]))
-            freqs1.append(chordm1.frequencies)
-        for chordm2 in sorted_chords_m2:
-            offsets2.append(float(chordm2.offset[0]))
-            durations2.append(float(chordm2.duration[0]))
-            freqs2.append(chordm2.frequencies)
-        integral_score = get_dictionary_for_f_x(offsets1, offsets2, durations1, durations2, freqs1, freqs2, float(measure_element1.duration[0]))
+    if len(measure_element1.duration) > 0 and len(measure_element2.duration) > 0:
+        if measure_element1.duration[0] == measure_element2.duration[0]:
+            offsets1 = []
+            offsets2 = []
+            durations1 = []
+            durations2 = []
+            freqs1 = []
+            freqs2 = []
+            for chordm1 in sorted_chords_m1:
+                offsets1.append(float(chordm1.offset[0]))
+                durations1.append(float(chordm1.duration[0]))
+                freqs1.append((chordm1.frequencies, chordm1.offset[0]))
+            for chordm2 in sorted_chords_m2:
+                offsets2.append(float(chordm2.offset[0]))
+                durations2.append(float(chordm2.duration[0]))
+                freqs2.append((chordm2.frequencies, chordm2.offset[0]))
+            integral_score = get_dictionary_for_f_x(offsets1, offsets2, durations1, durations2, freqs1, freqs2, float(measure_element1.duration[0]))
 
     return integral_score
 
-    # for elem in sorted_chords_m1:
-    #     cprint(elem.offset, "green")
-    #     cprint(max(elem.duration), "blue")
-    # for elem in sorted_chords_m2:
-    #     cprint(elem.offset, "yellow")
-    #     cprint(max(elem.duration), "cyan")
-
 def get_dictionary_for_f_x(offsets1, offsets2, durations1, durations2, freqs1, freqs2, m_duration):
+    # print(freqs1)
+    # print(freqs2)
     dict1, tup_list1 = compose_dict(offsets1, durations1, freqs1, m_duration)
     dict2, tup_list2 = compose_dict(offsets2, durations2, freqs2, m_duration)
     tup_list1.extend(tup_list2)
+    print(tup_list1)
     final_composed = get_final_list(tup_list1)
+    cprint(final_composed, "red") 
     sum_integral = 0
   
+    for i, part in enumerate(final_composed):
+        cprint(part, "green")
+        if i > 0:
+            if isinstance(part[2], tuple):
+                for chord_freqs_and_offs in part[2]:
+                    cprint(chord_freqs_and_offs, "yellow")
+
     for part in final_composed:
         if isinstance(part[2], tuple):
-            sum_integral += abs(quad(lambda x: f(x, part[2][0]) - g(x, part[2][1]), part[0], part[1])[0])
+            # sum_integral += abs(quad(lambda x: f(x, part[2][0]) - g(x, part[2][1]), part[0], part[1], limit=50)[0])
+            sum_integral += quad(diff_f, part[0], part[1], args=(part[2][0], part[2][1]), limit=100)[0]
         else:
-           sum_integral += abs(quad(lambda x: f(x, part[2]) - g(x, part[2]), part[0], part[1])[0])
-            
+        #    sum_integral += abs(quad(lambda x: f(x, part[2]) - g(x, part[2]), part[0], part[1], limit=50)[0])
+            sum_integral += quad(diff_f, part[0], part[1], args=(part[2], part[2]), limit=100)[0]
+
+    
     return sum_integral
-    # Print the resulting dictionary
     
 def compose_dict(list_offsets: list, list_durations: list, list_values: list, max_duration):
     dict = {}
@@ -312,19 +343,45 @@ def get_final_list(extended_list):
         end[r[1]].add(str(r[2]))
 
     current_ranges = set()
-
     final_list = []
 
     for e1, e2 in zip(endpoints[:-1], endpoints[1:]):
         current_ranges.difference_update(end[e1])
         current_ranges.update(start[e1])
-        final_list.append([e1, e2, ast.literal_eval(','.join(current_ranges))])
-        # print(e1, e2, ','.join(current_ranges))
+        if current_ranges == set():
+            current_ranges.add('0')
+            final_list.append([e1, e2, ast.literal_eval(','.join(current_ranges))])
+        else:
+            final_list.append([e1, e2, ast.literal_eval(','.join(current_ranges))]) 
     return final_list
 
 def get_c_values(obj_values, offset, duration):
     return obj_values if float(offset) == 0.0 else 0
 
-# Print the resulting dictionary
 
-get_element_similarity(measure_object_list[:20], 1)
+get_element_similarity(measure_object_list[:5], 1)
+
+
+
+
+# cprint(abs(quad(lambda x: f(x, ["1", "2.5"]) - g(x, ["2", "0.666"]), 0, 3, limit=100)[0]), "yellow")
+# cprint(abs(quad(diff_f, 0, 3, args=(["1", "2.5"], ["2", "0.666"]), limit=100)[0]), "red")
+
+
+# cprint(quad(f, 0, 3, args=["1", "2.5"]), "blue")
+# cprint(quad(g, 0, 3, args=["2", "0.666"]), "red")
+# cprint(quad(f, 0, 3, args=["1", "2.5"])[0] - quad(g, 0, 3, args=["2", "0.666"])[0], "green")
+
+# x = smp.symbols('x', real=True)
+# h = smp.sin(2*smp.pi*x) + smp.sin(2*smp.pi*2.5*x)
+# t = smp.sin(2*smp.pi*2*x) + smp.sin(2*smp.pi*0.666*x)
+
+# cprint(smp.integrate(f(x, ["1", "2.5"]), (x, 0, 6)))
+
+# i1 = smp.integrate(h, (x, 0, 3))
+# i2 = smp.integrate(h, (x, 0, 3))
+# i3 = smp.integrate(h-t, (x, 0 ,3))
+# print(i1)
+# print(i2)
+# print(i1-i2)
+# print(i3.evalf())
--- model/trainer_v2.py
+++ model/trainer_v2.py
@@ -4,7 +4,7 @@ import networkx as nx
 import torch
 
 graph = Graph()
-graph.parse("scoreportal/ontology.ttl", format="turtle")
+graph.parse("scoreportal/ontologyv2.ttl", format="turtle")
 
 nx_graph = nx.Graph()
 
@@ -14,8 +14,10 @@ for s, p, o in graph:
     nx_graph.add_edge(s, o)
 
 node2vec = Node2Vec(nx_graph, dimensions=64, walk_length=30, num_walks=200, workers=1)
-
+print("Node2ec initialized")
 model = node2vec.fit(window=10, min_count=1, batch_words=4)
-
-model.wv.save_word2vec_format('./modelv2emb.emb')
-model.save('./modelv2.model')
+print("Fit completed, starting save...")
+model.wv.save_word2vec_format('./modelv3emb.emb')
+print("Embeddings saved, starting saving model")
+model.save('./modelv3.model')
+print("Model saved!")
